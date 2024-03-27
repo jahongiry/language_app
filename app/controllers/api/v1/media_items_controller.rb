2b589ba59233e175ca2caff4d0548d93ae067f78
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-03-27 14:02:41 +0500

    Add cors settings

--- app/controllers/api/v1/media_items_controller.rb
+++ app/controllers/api/v1/media_items_controller.rb
@@ -31,58 +31,6 @@ module Api
         render json: @media_item
       end
 
-     # POST /api/v1/lessons/:lesson_id/media_items
-      def create
-        if params[:media_item][:media_type] == 'link'
-          # If it's a link, simply save it without processing
-          @media_item = @lesson.media_items.build(media_type: 'link', media_link: params[:media_item][:media_link])
-        elsif params[:media_item][:media_type] == 'image'
-          # If it's an image, extract the uploaded file
-          uploaded_file = params[:media_item][:media_link]
-
-          # Save the uploaded file and store its URL in media_link
-          if uploaded_file
-            @media_item = @lesson.media_items.build(media_type: 'image')
-            @media_item.media_link = save_uploaded_file(uploaded_file)
-          else
-            render json: { error: 'Image file is required' }, status: :unprocessable_entity
-            return
-          end
-        else
-          render json: { error: 'Invalid media type' }, status: :unprocessable_entity
-          return
-        end
-
-        if @media_item.save
-          render json: @media_item, status: :created
-        else
-          render json: @media_item.errors, status: :unprocessable_entity
-        end
-      end
-
-
-      def save_uploaded_file(uploaded_file)
-        # Generate a unique filename for the uploaded file
-        filename = "#{SecureRandom.hex(10)}_#{uploaded_file.original_filename}"
-        
-        # Specify the directory where you want to save the uploaded files
-        upload_directory = Rails.root.join('public', 'uploads')
-        
-        # Ensure that the directory exists, otherwise create it
-        FileUtils.mkdir_p(upload_directory) unless File.directory?(upload_directory)
-        
-        # Build the full path to save the file
-        file_path = File.join(upload_directory, filename)
-        
-        # Write the uploaded file to the specified path
-        File.open(file_path, 'wb') do |file|
-          file.write(uploaded_file.read)
-        end
-        
-        # Return the URL of the saved file
-        "/uploads/#{filename}"
-      end
-
       # PATCH/PUT /api/v1/lessons/:lesson_id/media_items/:id
       def update
         if @media_item.update(media_item_params)
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'https://dashboard-steel-phi.vercel.app', 'https://sooyaa.uz'
 
     resource '*',
       headers: :any,
